6e951028ece9a8746591f920bcc4baa9a0484e17
Liverpool4ik <liverpool4ik@proton.me>
2024-04-12 12:57:53 +0300

    had done TWO playLists

--- player.js
+++ player.js
@@ -2,38 +2,48 @@
 const playListHipHop = {
 	playListID: "1",
 	headerBlock: {
-		imageCoverUrl: "./images/mainCoverImage.jpg",
+		imageCoverUrl: "./images/HipHopPlayListImages/mainCoverImage.jpg",
 		topWord: "PlayList",
-		title: "Rap Hits 1990s",
+		title: "Hip Hop Hits",
 		tracksInfo: {
 			tracksCount: 4,
 			tracksDurationInSeconds: 733,
-			trackNames: "Eminem, 50cent, Lloyd Banks and others",
+			trackTitles: "Eminem, 50cent, Lloyd Banks and others",
 		},
 	},
 	tracksBlock: [
 		{
-			coverTrackImg: "./images/image-1.jpg",
-			artistName: "Public Enemy",
-			trackName: "Fight the Power",
+			trackId: "11",
+			coverTrackImg: "./images/HipHopPlayListImages/image-1.jpg",
+			trackFileUrl:
+				"/musicTracks/playListHipHop/Eminem_-_Rap_God_[mp3pulse.ru]_sample.mp3",
+			artistName: "Eminem",
+			trackTitle: "Rap God",
 			isPopular: true,
 		},
 		{
-			coverTrackImg: "./images/image-2.jpg",
-			artistName: "Vanilla Ice",
-			trackName: "Ice Ice Baby",
+			trackId: "11",
+			coverTrackImg: "./images/HipHopPlayListImages/image-2.jpg",
+			trackFileUrl: "/musicTracks/playListHipHop/track37695032.mp3",
+			artistName: "50 cent",
+			trackTitle: "In da Club",
 			isPopular: true,
 		},
 		{
-			coverTrackImg: "./images/image-3.jpg",
-			artistName: "MC Hammer",
-			trackName: "You Can't touch this",
+			trackId: "11",
+			coverTrackImg: "./images/HipHopPlayListImages/image-3.jpg",
+			trackFileUrl:
+				"/musicTracks/playListHipHop/DMX - X Gon' Give It To Ya (192kbps).mp3",
+			artistName: "DMX",
+			trackTitle: "X Gon'Give It to YA",
 			isPopular: false,
 		},
 		{
-			coverTrackImg: "./images/image-4.jpg",
-			artistName: "Brand Nubian",
-			trackName: "Brand Nubian",
+			trackId: "11",
+			coverTrackImg: "./images/HipHopPlayListImages/image-4.jpg",
+			trackFileUrl: "/musicTracks/playListHipHop/4.mp3",
+			artistName: "Eminem feat 50 cent, Cashis, Lloyd Banks",
+			trackTitle: "You Don't Know",
 			isPopular: false,
 		},
 	],
@@ -48,32 +58,40 @@ const playListRapHits1990 = {
 		tracksInfo: {
 			tracksCount: 4,
 			tracksDurationInSeconds: 733,
-			trackNames: "Eminem, 50cent, Lloyd Banks and others",
+			trackTitles: "Eminem, 50cent, Lloyd Banks and others",
 		},
 	},
 	tracksBlock: [
 		{
+			trackId: "11",
 			coverTrackImg: "./images/image-1.jpg",
+			trackFileUrl: "/musicTracks/playListRapHits1990/track-1.mp3",
 			artistName: "Public Enemy",
-			trackName: "Fight the Power",
+			trackTitle: "Fight the Power",
 			isPopular: true,
 		},
 		{
+			trackId: "11",
 			coverTrackImg: "./images/image-2.jpg",
+			trackFileUrl: "/musicTracks/playListRapHits1990/track-2.mp3",
 			artistName: "Vanilla Ice",
-			trackName: "Ice Ice Baby",
+			trackTitle: "Ice Ice Baby",
 			isPopular: true,
 		},
 		{
+			trackId: "11",
 			coverTrackImg: "./images/image-3.jpg",
+			trackFileUrl: "/musicTracks/playListRapHits1990/track-3.mp3",
 			artistName: "MC Hammer",
-			trackName: "You Can't touch this",
+			trackTitle: "You Can't touch this",
 			isPopular: false,
 		},
 		{
+			trackId: "11",
 			coverTrackImg: "./images/image-4.jpg",
+			trackFileUrl: "/musicTracks/playListRapHits1990/track-4.mp3",
 			artistName: "Brand Nubian",
-			trackName: "Brand Nubian",
+			trackTitle: "Brand Nubian",
 			isPopular: false,
 		},
 	],
@@ -81,28 +99,67 @@ const playListRapHits1990 = {
 
 //render
 
-const playListTitleElement = document.createElement("h1");
-playListTitleElement.append(playListRapHits1990.headerBlock.title);
-document.body.append(playListTitleElement);
+// const playListTitleElement = document.createElement("h1");
+// playListTitleElement.append(playListRapHits1990.headerBlock.title);
+// document.body.append(playListTitleElement);
 
-const playListCoverImageElement = document.createElement("img");
-playListCoverImageElement.src = playListRapHits1990.headerBlock.imageCoverUrl;
-document.body.append(playListCoverImageElement);
+// const playListCoverImageElement = document.createElement("img");
+// playListCoverImageElement.src = playListRapHits1990.headerBlock.imageCoverUrl;
+// document.body.append(playListCoverImageElement);
 
-const trackListElement = document.createElement("ul");
-for (let i = 0; i < trackListElement.length; i++) {
-	const trackElement = document.createElement("li");
-	const trackCoverElement = document.createElement("img");
-	trackCoverElement.src = playListRapHits1990.tracksBlock[i].coverTrackImg;
-	trackCoverElement.style.width = "50px";
-	trackCoverElement.style.height = "50px";
-	trackElement.append(trackCoverElement);
-	trackElement.append(
-		playListRapHits1990.tracksBlock[i].artistName +
-			"-" +
-			playListRapHits1990.tracksBlock[i].trackName
-	);
-	trackListElement.append(trackElement);
+// const trackListElement = document.createElement("ul");
+// for (let i = 0; i < playListRapHits1990.tracksBlock.length; i++) {
+// 	const trackElement = document.createElement("li");
+// 	const trackCoverElement = document.createElement("img");
+// 	trackCoverElement.src = playListRapHits1990.tracksBlock[i].coverTrackImg;
+// 	trackCoverElement.style.width = "50px";
+// 	trackCoverElement.style.height = "50px";
+// 	trackElement.append(trackCoverElement);
+// 	trackElement.append(
+// 		playListRapHits1990.tracksBlock[i].artistName +
+// 			" - " +
+// 			playListRapHits1990.tracksBlock[i].trackTitle
+// 	);
+// 	trackListElement.append(trackElement);
+// }
+
+// document.body.append(trackListElement);
+
+function renderPlayList(anyPlayList) {
+	renderPlayListHeader(anyPlayList.headerBlock);
+
+	const trackListElement = document.createElement("ul");
+	for (let i = 0; i < anyPlayList.tracksBlock.length; i++) {
+		const trackElement = createAnyTrack(anyPlayList.tracksBlock[i]);
+		trackListElement.append(trackElement);
+	}
+	document.body.append(trackListElement);
 }
 
-document.body.append(trackListElement);
+function renderPlayListHeader(anyPlayListHeader) {
+	const playListTitleElement = document.createElement("h1");
+	playListTitleElement.append(anyPlayListHeader.title);
+	document.body.append(playListTitleElement);
+
+	const playListCoverImageElement = document.createElement("img");
+	playListCoverImageElement.src = anyPlayListHeader.imageCoverUrl;
+	document.body.append(playListCoverImageElement);
+}
+function createAnyTrack(anyTrack) {
+	const trackElement = document.createElement("li");
+	const trackCoverElement = document.createElement("img");
+	trackCoverElement.src = anyTrack.coverTrackImg;
+	trackCoverElement.style.width = "50px";
+	trackCoverElement.style.height = "50px";
+
+	const audioElement = document.createElement("audio");
+	audioElement.src = anyTrack.trackFileUrl;
+	audioElement.controls = true;
+	trackElement.append(trackCoverElement);
+	trackElement.append(anyTrack.artistName + " - " + anyTrack.trackTitle);
+	trackElement.append(audioElement);
+	return trackElement;
+}
+
+renderPlayList(playListHipHop);
+renderPlayList(playListRapHits1990);
